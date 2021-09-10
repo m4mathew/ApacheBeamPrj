f7bdf4965fc0bda48fe2194ae3b88eaa0338c3f4
m4mathew <007.mathew@gmail.com>
2021-09-10 10:52:50 +0530

    Added code chnages  to mask digits by '*'

--- src/main/java/apacheBeamPrj/Dataflow.java
+++ src/main/java/apacheBeamPrj/Dataflow.java
@@ -2,6 +2,7 @@ package apacheBeamPrj;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.Regex;
 import org.apache.beam.sdk.values.PCollection;
 
 public class Dataflow {
@@ -9,7 +10,8 @@ public class Dataflow {
 	public static void main(String[] args) {
 		
 		Pipeline pipeline = Pipeline.create();
-		PCollection<String> outPCollection = pipeline.apply(TextIO.read().from("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/input.csv"));
+		PCollection<String> lines = pipeline.apply(TextIO.read().from("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/input.csv"));
+		PCollection<String> outPCollection = lines.apply(Regex.replaceAll("\\d(?=\\d{3})","*"));
 		outPCollection.apply(TextIO.write().to("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/output.csv"));
 	
 		pipeline.run();
