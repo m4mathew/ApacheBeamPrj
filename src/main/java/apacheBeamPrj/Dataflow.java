b613bafaffcb51ce98f5451055703080fb67d020
m4mathew <007.mathew@gmail.com>
2021-09-13 15:28:14 +0530

    Added PipelineOptions

--- src/main/java/apacheBeamPrj/Dataflow.java
+++ src/main/java/apacheBeamPrj/Dataflow.java
@@ -2,15 +2,27 @@ package apacheBeamPrj;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Regex;
 import org.apache.beam.sdk.values.PCollection;
 
 public class Dataflow {
-
+	public static interface InputOptions extends PipelineOptions {
+		  @Description("Path of the file to read from")
+		  @Default.String("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/input.csv")
+		  String getInputFile();
+		  void setInputFile(String value);
+		  
+		}
+	
 	public static void main(String[] args) {
-		
-		Pipeline pipeline = Pipeline.create();
-		PCollection<String> lines = pipeline.apply(TextIO.read().from("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/input.csv"));
+		InputOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+			      .as(InputOptions.class);
+		Pipeline pipeline = Pipeline.create(options);
+		PCollection<String> lines = pipeline.apply(TextIO.read().from(options.getInputFile()));
 		PCollection<String> outPCollection = lines.apply(Regex.replaceAll("\\d(?=\\d{3})","*"));
 		outPCollection.apply(TextIO.write().to("/home/mmathew/eclipse-newworkspace/ApacheBeamPrj/src/main/java/apacheBeamPrj/output.csv"));
 	
